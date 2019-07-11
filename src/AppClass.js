c677ae8725db7fe3d3ad7145eab5e3cc5c460748
Alan Perez <alanp9six@gmail.com>
2019-07-10 23:27:31 -0500

    :sparkles: Creating class based component then refactoring it to a Functional Component that uses Hooks

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,21 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class AppClass extends Component {
- state: {
-   count: 0
- }
+class App extends Component {
+
+  state = {
+    count: 0,
+    isOn: false
+  }
 
  incrementCount = () => {
-   this.setState({
-     count:this.state.count + 1
-   })
+   this.setState(prevState => ({
+    count: prevState.count + 1
+   }))
  }
+
+ toggleLight = () => {
+   this.setState(prevState => ({
+     isOn: !prevState.isOn
+   }))
+  }
 
  render() {
    return (
-     <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+     <>
+     <h2>Counter</h2>
+     <button onClick={this.incrementCount}>
+       I was clicked {this.state.count} times
+     </button>
+
+     <h2>Toggle Light</h2>
+     <div 
+      style={{
+        height:'50px',
+        width: '50px',
+        background: this.state.isOn ? "yellow" : "grey"
+      }}
+      onClick={this.toggleLight}
+      />
+     </>
    )
  }
 }
 
-export default AppClass
+export default App
